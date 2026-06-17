4fffa575254238752dc65c4d615781d1cba77ef8
martin1988-asia <mbweti@gmail.com>
2026-06-17 04:43:19 +0200

    Deploy fresh build from main commit 46369cc

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "566899329" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3476904321" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
